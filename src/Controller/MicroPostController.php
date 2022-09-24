f2c17c8abd30888efd4e27bc5683c8821a7610c1
Andy Gorbould <gorbould@mail.com>
2022-09-24 16:44:29 +0200

    security.yaml inherritance not working right now, fix it!

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
-use DateTime;
+// use DateTime;                // not needed???
 use App\Entity\Comment;
 use App\Entity\MicroPost;
 use App\Form\CommentType;
 use App\Form\MicroPostType;
 use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,6 +27,7 @@ class MicroPostController extends AbstractController
 
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[IsGranted(MicroPost::VIEW, 'post')]
     public function showOne(MicroPost $post): Response          // MicroPost called because sensio uses that in param converter for single cases. Use the MicroPostRepository injection for multiple cases (above) // https://symfony.com/bundles/SensioFrameworkExtraBundle/current/annotations/converters.html
     {
         return $this->render('micro_post/show.html.twig', [
@@ -34,6 +36,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]                      // ⛔ Deny access to this page if not logged in
     public function add(Request $request, MicroPostRepository $posts): Response
     {
         $form = $this->createForm(MicroPostType::class, new MicroPost());
@@ -42,7 +45,7 @@ class MicroPostController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
-            $post->setCreated(new DateTime());
+            // $post->setCreated(new DateTime());
             $post->setAuthor($this->getUser());
             $posts->add($post, true);           // 📌 remember 'true' for flush on entity manager (repository)
 
@@ -60,11 +63,14 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    #[IsGranted(MicroPost::EDIT, 'post')]
     public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
     {
         $form = $this->createForm(MicroPostType::class, $post);     // class & data
 
         $form->handleRequest($request);
+
+        // $this->denyAccessUnlessGranted(MicroPost::EDIT, $post);          // ⭐⭐⭐ is it this??
 
         if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
@@ -85,6 +91,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    #[IsGranted('ROLE_COMMENTER')]
     public function addComment(MicroPost $post, Request $request, CommentRepository $comments): Response
     {
         $form = $this->createForm(CommentType::class, new Comment());     // class & data
